bca693318114167eb0d9437b31bf5c0ef6869307
JsonB001 <89361656+JsonB001@users.noreply.github.com>
2021-08-23 21:46:35 -0700

    Add files via upload

---
+++ haiti.js
@@ -0,0 +1,10 @@
+function myMenu() {
+   document.getElementById("menuList").style.display="block";
+   document.getElementById("icon1").style.display="none";
+   document.getElementById("icon2").style.display="block";
+}
+function myMenu2() {
+    document.getElementById("menuList").style.display="none";
+    document.getElementById("icon2").style.display="none";
+    document.getElementById("icon1").style.display="block";
+}
